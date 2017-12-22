1d24566d19ad432ae5783bd7c7340caec959dc0e
liszek <mateusz.lichon@gmail.com>
2017-12-22 13:01:50 +0100

    init project

---
+++ src/main/java/pl/coderslab/warsztat6/app/AppConfig.java
@@ -0,0 +1,86 @@
+package pl.coderslab.warsztat6.app;
+
+import java.util.Locale;
+
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@Configuration
+@ComponentScan(basePackages = { "pl.coderslab.warsztat6.bean", "pl.coderslab.warsztat6.controller", "pl.coderslab.warsztat6.entity", "pl.coderslab.warsztat6.dao", "pl.coderslab.warsztat6.repository.controller"  })
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages= {"pl.coderslab.warsztat6.repository"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory() {
+		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+		emfb.setPersistenceUnitName("warsztat6");
+		return emfb;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager tm = new JpaTransactionManager(emf);
+		return tm;
+	}
+	
+/*	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(authorConverter());
+		registry.addConverter(categoryConverter());
+	}
+
+	@Bean
+	public AuthorConverter authorConverter() {
+		return new AuthorConverter();
+	}
+
+	@Bean
+	public CategoryConverter categoryConverter() {
+		return new CategoryConverter();
+	}*/
+	
+	@Bean(name = "localeResolver")
+	public LocaleContextResolver getLocaleContextResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+		return localeResolver;
+	}
+	
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+}
---
+++ src/main/java/pl/coderslab/warsztat6/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.coderslab.warsztat6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("")
+	public String home() {
+		return "home";
+	}
+}
